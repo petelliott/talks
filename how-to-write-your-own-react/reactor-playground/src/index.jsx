8e0eb1c058059b9889d241d7bce85689af6fe875
Peter Elliott <pelliott@serenityos.org>
2023-10-13 01:45:17 -0600

    finish first draft rendering

--- how-to-write-your-own-react/reactor-playground/src/index.jsx
+++ how-to-write-your-own-react/reactor-playground/src/index.jsx
@@ -1,4 +1,6 @@
 import * as Reactor from "reactor";
 
-console.log(<div style="color:green"><h1>hey</h1><p>paragraph</p></div>);
-// Reactor.render(document.getElementById("root"), <div style="color:green"><h1>hey</h1><p>paragraph</p></div>);
+const App = (props) =>
+<div style="color:green"><h1>hey</h1><p>paragraph</p></div>;
+
+Reactor.render(document.getElementById("root"), <App/>);
--- how-to-write-your-own-react/reactor/index.js
+++ how-to-write-your-own-react/reactor/index.js
@@ -1,12 +1,14 @@
-import { propsEqual, arraySetDifference} from "./util";
+import { propsEqual, arraySetDifference } from "./util";
 
 export const render = (element, component) =>
-    element.appendChild(component);
+    element.appendChild(freshRenderComponent(component).element);
 
 export const component = (renderer, props) => ({
     type: renderer,
     props: props,
 });
+
+const textnode = Symbol("textnode");
 
 const normalizeChildren = (children) => {
     if (children === null || children === undefined)
@@ -18,14 +20,56 @@ const normalizeChildren = (children) => {
     return children;
 };
 
-const freshRenderComponent = (component, tree) => {
+const freshRenderComponent = (component) => {
+    if (typeof component == "string") {
+        return {
+            typ: textnode,
+            element: document.createTextNode(component),
+            value: component,
+        };
+    }
 
+    if (typeof component.type === "function") {
+        const child = freshRenderComponent(component.type(component.props));
+        return {
+            type: component.type,
+            props: component.props,
+            element: child.element,
+            children: child,
+        };
+    }
+
+    const { children, ...otherProps } = component.props;
+    const element = document.createElement(component.type);
+    for (const [key, value] of Object.entries(otherProps)) {
+        element.setAttribute(key, value);
+    }
+
+    const childnodes = normalizeChildren(children).map(freshRenderComponent);
+    for (const child of childnodes) {
+        element.appendChild(child.element);
+    }
+
+    return {
+        type: component.type,
+        props: component.props,
+        element: element,
+        children: childnodes,
+    };
 };
 
 const reconcileComponent = (component, tree) => {
+    if (typeof component === "string") {
+        if (tree.value !== component) {
+            tree.element.nodeValue = component;
+            tree.value = component;
+        }
+        return tree;
+    }
+
     if (typeof component.type === "function") {
         tree.props = component.props;
-        tree.children = renderComponent(component.type(props), tree.children);
+        tree.children = rerenderComponent(component.type(props), tree.children);
         tree.element = tree.children.element;
         return tree;
     }
@@ -33,6 +77,7 @@ const reconcileComponent = (component, tree) => {
     const { treeChildren, ...treeProps } = tree.props;
     const { componentChildren, ...componentProps } = component.props;
 
+    // reconcile props
     const oldkeys = Object.keys(treeProps);
     const newkeys = Object.keys(componentProps);
 
@@ -47,15 +92,32 @@ const reconcileComponent = (component, tree) => {
             tree.element.setAttribute(attr, componentProps[attr]);
         }
     }
+    tree.props = component.props;
 
+    // reconcile children
     componentChildren = normalizeChildren(componentChildren);
 
-    let i = 0;
-    for (const )
+    const newchildren = [];
+    let i;
+    for (i = 0; i < treeChildren.length && i < componentChildren.length; ++i) {
+        newchildren.push(rerenderComponent(componentChildren[i], treeChildren[i]));
+    }
+
+    // remove excess tree children from the dom
+    for (; i < treeChildren.length; ++i) {
+        treeChildren[i].element.remove();
+    }
+
+    // add extra new children
+    for (; i < componentChildren[i]; ++i) {
+        newchildren.push(freshRenderComponent(componentChildren[i]));
+    }
+    tree.children = newchildren;
+    return tree;
 };
 
 const rerenderComponent = (component, tree) => {
-    if (tree === null || component.type !== tree.type)
+    if (tree === null || (typeof component === "string" && tree.typ !== textnode) || component.type !== tree.type)
         return freshRenderComponent(component);
 
     if (propsEqual(component.props, tree.props))
@@ -63,24 +125,3 @@ const rerenderComponent = (component, tree) => {
 
     return reconcileComponent(component, tree);
 };
-
-/*
-    if (typeof renderer === "string") {
-        const { children, ...otherProps } = props;
-        const element = document.createElement(renderer, {});
-        for (const [key, value] of Object.entries(otherProps)) {
-            element.setAttribute(key, value);
-        }
-        for (const child of children) {
-            if (typeof child === "object") {
-                element.appendChild(child);
-            } else if (typeof child === "string") {
-                element.appendChild(document.createTextNode(child));
-            }
-        }
-        return element;
-    } else {
-        return renderer(props);
-    }
-}
-*/
